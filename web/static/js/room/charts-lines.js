e36bafaabe21de3d9b8df914e01c91b6e648416d
SunWintor <793815374@qq.com>
2022-08-23 23:00:50 +0800

    # 初始化项目 ## 游戏流程 - 完善了游戏图表展示

--- web/static/js/room/charts-lines.js
+++ web/static/js/room/charts-lines.js
@@ -1,78 +1,120 @@
 
-lineConfig = {
-  type: 'line',
-  data: {
-    labels: chartsLabels,
-    datasets: [
-      {
-        label: 'Organic',
+historyLine = null
+moneyLine = null
+
+function initDatasets(map) {
+  datasets = []
+  $.each(map,function(key,value) {
+    datasets.push(value)
+  });
+  return datasets
+}
+
+function initLine() {
+  if (Object.keys(playerHistoryMap).length > 0 && historyLine == null) {
+    historyConfig = {
+      type: 'line',
+      data: {
+        labels: [],
+        datasets: initDatasets(playerHistoryMap),
+      },
+      options: {
+        responsive: true,
         /**
-         * These colors come from Tailwind CSS palette
-         * https://tailwindcss.com/docs/customizing-colors/#default-color-palette
-         */
-        backgroundColor: '#fef9c3',
-        borderColor: '#fef9c3',
-        data: [43, 48, 40, 108, 0, 0, 70],
-        fill: false,
-      },
-      {
-        label: 'Paid',
-        fill: false,
-        backgroundColor: '#7e3af2',
-        borderColor: '#7e3af2',
-        data: [24, 50, 64, 74, 52, 51, 65],
-      },
-      {
-        label: 'Paid3',
-        fill: false,
-        backgroundColor: '#666666',
-        borderColor: '#666666',
-        data: [24, 1, 2, 3, 4, 5, 6],
-      },
-    ],
-  },
-  options: {
-    responsive: true,
-    /**
-     * Default legends are ugly and impossible to style.
-     * See examples in charts.html to add your own legends
-     *  */
-    legend: {
-      display: false,
-    },
-    tooltips: {
-      mode: 'index',
-      intersect: false,
-    },
-    hover: {
-      mode: 'nearest',
-      intersect: true,
-    },
-    scales: {
-      x: {
-        display: true,
-        scaleLabel: {
-          display: true,
-          labelString: 'Round',
+         * Default legends are ugly and impossible to style.
+         * See examples in charts.html to add your own legends
+         *  */
+        legend: {
+          display: false,
+        },
+        tooltips: {
+          mode: 'index',
+          intersect: false,
+        },
+        hover: {
+          mode: 'nearest',
+          intersect: true,
+        },
+        scales: {
+          x: {
+            display: true,
+            scaleLabel: {
+              display: true,
+              labelString: 'Round',
+            },
+          },
+          y: {
+            display: true,
+            scaleLabel: {
+              display: true,
+              labelString: 'Money',
+            },
+          },
         },
       },
-      y: {
-        display: true,
-        scaleLabel: {
-          display: true,
-          labelString: 'Money',
+    }
+    historyLine = new Chart(document.getElementById('donated_history'), historyConfig)
+  }
+  if (Object.keys(playerMoneyMap).length > 0 && moneyLine == null) {
+    moneyConfig = {
+      type: 'line',
+      data: {
+        labels: [],
+        datasets: initDatasets(playerMoneyMap),
+      },
+      options: {
+        responsive: true,
+        /**
+         * Default legends are ugly and impossible to style.
+         * See examples in charts.html to add your own legends
+         *  */
+        legend: {
+          display: false,
+        },
+        tooltips: {
+          mode: 'index',
+          intersect: false,
+        },
+        hover: {
+          mode: 'nearest',
+          intersect: true,
+        },
+        scales: {
+          x: {
+            display: true,
+            scaleLabel: {
+              display: true,
+              labelString: 'Round',
+            },
+          },
+          y: {
+            display: true,
+            scaleLabel: {
+              display: true,
+              labelString: 'Money',
+            },
+          },
         },
       },
-    },
-  },
+    }
+    moneyLine = new Chart(document.getElementById('money_history'), moneyConfig)
+  }
 }
 
 function flushCharts() {
-  const donated_history = document.getElementById('donated_history')
-  window.myLine = new Chart(donated_history, lineConfig)
+  initLine()
+  $.each(playerHistoryMap,function(key,value){
+    addData(historyLine, value, value.index)
+  });
+  $.each(playerMoneyMap,function(key,value){
+    addData(moneyLine, value, value.index)
+  });
 }
 
-setInterval(flushCharts,500)
+function addData(chart, dataset, datasetIndex) {
+  chart.data.datasets[datasetIndex].data = dataset.data;
+  chart.data.labels = chartsLabels;
+  chart.update();
+}
 
-const money_history = document.getElementById('money_history')
-window.myLine2 = new Chart(money_history, lineConfig)
+setInterval(flushCharts,3000)
